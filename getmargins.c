2f9cafa0b19f5f076a2e57ab59b2bf10df013b46
Arun Sahadeo <arunjamessahadeo@gmail.com>
2018-03-22 23:18:00 +0000

    Update script

--- getmargins.c
+++ getmargins.c
@@ -4,23 +4,56 @@
 
 int main(int argc, char *argv[])
 {
+    int marginValues[argc];
 
-	int marginValues[argc - 1];
+    for ( int i = 0; i < argc; i++ )
+    {
+            if ( i == 0 ) continue;
+            if (isdigit(*argv[i]))
+            {
+                int value = atoi(argv[i]);
+                marginValues[i] = value;
+            }
+    }
 
-	for ( int i = 0; i < argc; i++ )
-	{
-		if ( i == 0 ) continue;
-		if (isdigit(*argv[i]))
-		{
-			marginValues[i] = *argv[i];
-		}
-	}
+    size_t size = sizeof(marginValues) / sizeof(marginValues[0]);
+    size -= 1;
 
-	for ( int i = 0; i <= (sizeof(marginValues) / sizeof(marginValues[0])); i++ )
-	{
-		printf("index: %d\n", i);
-		printf("%d\n", marginValues[i]);
-	}
+    switch(size)
+    {
+        case 1:
+            printf("%s %d \n", "You entered:", marginValues[1]);
+            printf("%s\n", "This gives the following values:");
+            printf("%s %d%s\n", "Margin-top:", marginValues[1], "px");
+            printf("%s %d%s\n", "Margin-right:", marginValues[1], "px");
+            printf("%s %d%s\n", "Margin-bottom:", marginValues[1], "px");
+            printf("%s %d%s\n", "Margin-left:", marginValues[1], "px");
+            break;
+        case 2:
+            printf("%s %d, %d\n", "You entered:", marginValues[1], marginValues[2]);
+            printf("%s\n", "This gives the following values:");
+            printf("%s %d%s\n", "Margin-top:", marginValues[1], "px");
+            printf("%s %d%s\n", "Margin-right:", marginValues[2], "px");
+            printf("%s %d%s\n", "Margin-bottom:", marginValues[1], "px");
+            printf("%s%d%s\n", "Margin-left:", marginValues[2], "px");
+            break;
+        case 3:
+            printf("%s %d, %d, %d\n", "You entered:", marginValues[1], marginValues[2], marginValues[3]);
+            printf("%s\n", "This gives the following values:");
+            printf("%s %d%s\n", "Margin-top:", marginValues[1], "px");
+            printf("%s %d%s\n", "Margin-right:", marginValues[2], "px");
+            printf("%s %d%s\n", "Margin-bottom:", marginValues[3], "px");
+            printf("%s %d%s\n", "Margin-left:", marginValues[2], "px");
+            break;
+        case 4:
+            printf("%s %d, %d, %d, %d\n", "You entered:", marginValues[1], marginValues[2], marginValues[3], marginValues[4]);
+            printf("%s\n", "This gives the following values:");
+            printf("%s %d%s\n", "Margin-top:", marginValues[1], "px");
+            printf("%s %d%s\n", "Margin-right:", marginValues[2], "px");
+            printf("%s %d%s\n", "Margin-bottom:", marginValues[3], "px");
+            printf("%s %d%s\n", "Margin-left:", marginValues[4], "px");
+            break;
+    }
 
-	return 0;
+    return 0;
 }
